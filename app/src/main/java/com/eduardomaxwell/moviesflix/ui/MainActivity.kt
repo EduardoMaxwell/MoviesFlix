cd9b4211b4e91f717de3edfa8972527697c0afcf
Eduardo Maxwell <eduardomaxwell2011@gmail.com>
2021-10-07 21:38:40 -0400

    Initial config and added class model

--- app/src/main/java/com/eduardomaxwell/moviesflix/ui/MainActivity.kt
+++ app/src/main/java/com/eduardomaxwell/moviesflix/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.eduardomaxwell.moviesflix
+package com.eduardomaxwell.moviesflix.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.eduardomaxwell.moviesflix.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
